05b7c25fbe0940b9bde0b050375892da887b6690
Vybornyi <1vybornyi@gmail.com>
2022-08-06 17:30:39 +0300

    add

---
+++ lesson-7/task-2/src/list/createTask.js
@@ -0,0 +1,22 @@
+import { renderTasks } from './rendered.js';
+import { getItem, setItem } from './storage.js';
+import { createTask, getTasksList } from './tasksGateway.js';
+export const onCreateTask = () => {
+  const taskTitleInputElem = document.querySelector('.task-input');
+  const text = taskTitleInputElem.value;
+  if (!text) {
+    return;
+  }
+  taskTitleInputElem.value = '';
+  const newTask = {
+    text,
+    done: false,
+    createdDate: new Date().toISOString(),
+  };
+  createTask(newTask)
+    .then(() => getTasksList())
+    .then(newTasksList => {
+      setItem('tasksList', newTasksList);
+      renderTasks();
+    });
+};
